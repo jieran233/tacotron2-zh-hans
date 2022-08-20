10a756afb9a88f31d2e5b02247c5b053093d3432
jieran233 <jieranjiang233@gmail.com>
2022-08-20 15:47:45 +0800

    Update run.sh
    bugfix

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ siblings=$(grep -c '^processor' /proc/cpuinfo)  # https://stackoverflow.com/a/64
 export OMP_NUM_THREADS=$siblings
 export MKL_NUM_THREADS=$siblings
 export OPENBLAS_NUM_THREADS=$siblings
-python3 train.py -c ckpt.pt --output_directory=./output/ckpt --log_directory=./output/log
+python3 train.py -c ckpt.pt --output_directory=./output --log_directory=./log
