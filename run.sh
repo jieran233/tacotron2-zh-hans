b3cedfe9db1ada92518f432ba4d5c46b00046c0c
jieran233 <jieranjiang233@gmail.com>
2022-09-10 10:30:59 +0800

    update
    BLIS

--- run.sh
+++ run.sh
@@ -6,5 +6,10 @@ siblings=$(grep -c '^processor' /proc/cpuinfo)  # https://stackoverflow.com/a/64
 export OMP_NUM_THREADS=$siblings
 export MKL_NUM_THREADS=$siblings
 export OPENBLAS_NUM_THREADS=$siblings
+export OMP_NUM_THREADS=$siblings
+export MKL_NUM_THREADS=$siblings
+export BLIS_NUM_THREADS=$siblings
+# export OMP_PROC_BIND=spread
+export OPENBLAS_NUM_THREADS=$siblings
 
 python3 train.py -c ckpt.pt --output_directory=./output --log_directory=./log
